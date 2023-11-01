951d8aba06e1c531b703906152464f752e6363f9
SimonGideon <simongideon918@gmail.com>
2023-11-01 12:37:55 +0300

    Initial commit

--- db/schema.rb
+++ db/schema.rb
@@ -34,6 +34,10 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_01_083543) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
     t.index ["email"], name: "index_lecturers_on_email", unique: true
     t.index ["reset_password_token"], name: "index_lecturers_on_reset_password_token", unique: true
   end
@@ -49,6 +53,10 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_01_083543) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
     t.index ["email"], name: "index_students_on_email", unique: true
     t.index ["reset_password_token"], name: "index_students_on_reset_password_token", unique: true
   end
