c952b633263c81ac7090377d7278d8a404f29079
SimonGideon <simongideon918@gmail.com>
2023-11-07 16:32:24 +0300

    Initial commit

--- app/models/lecturer.rb
+++ app/models/lecturer.rb
@@ -6,4 +6,5 @@ class Lecturer < ApplicationRecord
   has_many :courses
   has_many :students, through: :courses
   has_many :lecturer_courses
+  has_many :attendances
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -5,4 +5,5 @@ class Student < ApplicationRecord
          :recoverable, :rememberable, :validatable, :confirmable
   has_and_belongs_to_many :courses
   has_many :lecturers, through: :courses
+  has_many :attendances
 end
