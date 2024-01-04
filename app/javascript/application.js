498cc114a35a2d86434eb94c5367f034f29d658e
SimonGideon <simongideon918@gmail.com>
2024-01-04 23:44:17 +0300

    Initial commit

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,4 +1,6 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+import "popper"
+import "bootstrap"
 import "@hotwired/turbo-rails"
 import "controllers"
 import "../assets/javascripts/qr-scanner"
--- config/importmap.rb
+++ config/importmap.rb
@@ -1,5 +1,6 @@
 # Pin npm packages by running ./bin/importmap
-
+pin "popper", to: "popper.js", preload: true
+pin "bootstrap", to: "bootstrap.min.js", preload: true
 pin "application", preload: true
 pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
