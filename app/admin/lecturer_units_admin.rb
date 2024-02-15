33dbeb9e2e9b71f1ee651041fbd845191b7d1bda
SimonGideon <simongideon918@gmail.com>
2024-02-15 17:13:47 +0300

    fix: lecturer_unit styles

--- app/admin/lecturer_units_admin.rb
+++ app/admin/lecturer_units_admin.rb
@@ -29,12 +29,12 @@ Trestle.resource(:lecturer_units) do
   form do |lecturer_unit|
     select :lecturer_id, options_for_select(Lecturer.all.map { |lecturer| [lecturer.name, lecturer.id] }), label: "Lecturer"
     select :course_id, options_for_select(Course.all.map { |course| [course.course_name, course.id] }), label: "Course"
-    concat(content_tag(:ol) do
+    concat(content_tag(:ol) do # Added class list-unstyled for removing default list styles
       lecturer_unit.students_courses.each do |students_course|
         student = students_course.student # Assuming there is a student association
-        concat(content_tag(:li) do
+        concat(content_tag(:li, class: "d-flex justify-content-between align-items-center mb-2 ") do # Added classes for flex layout and margin-bottom
           concat(content_tag(:span, student.name)) # Assuming the student has a name attribute
-          concat(button_to("Delete", "/students/#{student.id}/students_courses/#{students_course.id}", method: :delete, data: { confirm: "Are you sure you want to delete this student?" }))
+          concat(button_to("Delete", "/students/#{student.id}/students_courses/#{students_course.id}", method: :delete, data: { confirm: "Are you sure you want to delete this student?" }, class: "btn btn-danger"))
         end)
       end
     end)
