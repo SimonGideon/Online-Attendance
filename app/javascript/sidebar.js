0eea8b4c661c2419187c7a16ebf336b3de0e4185
SimonGideon <simongideon918@gmail.com>
2024-02-27 20:21:03 +0300

    Mobile student dahsboard

--- app/javascript/sidebar.js
+++ app/javascript/sidebar.js
@@ -1,17 +1,21 @@
 const expand_btn = document.querySelector(".expand-btn");
 const open_menu = document.querySelector(".open-menu");
 const close_menu = document.querySelector(".close-menu");
+const sidebar = document.querySelector(".sidebar")
 
 
 // mobile nav open 
 open_menu.addEventListener("click", () => {
   open_menu.classList.toggle("hide-menu-btn");
   close_menu.classList.toggle("show")
+  sidebar.classList.toggle("show")
+  expand_btn.classList.toggle("hide-menu-btn")
 });
 
 close_menu.addEventListener("click", () => {
   open_menu.classList.remove("hide-menu-btn");
   close_menu.classList.remove("show");
+  sidebar.classList.remove("show")
 });
 
 let activeIndex;
